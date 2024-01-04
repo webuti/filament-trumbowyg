5a3561d273cb86015ce806b46aa057cf9a28a270
omer <qmerfp@me.com>
2024-01-04 22:25:41 +0300

    fix

--- resources/views/trumbowyg.blade.php
+++ resources/views/trumbowyg.blade.php
@@ -9,9 +9,7 @@ $editorId = strtolower(str_replace('.', '-', $getId()));
     :label-sr-only="$isLabelHidden()"
     :helper-text="$getHelperText()"
     :hint="$getHint()"
-    :hint-action="$getHintAction()"
-    :hint-color="$getHintColor()"
-    :hint-icon="$getHintIcon()"
+ 
     :required="$isRequired()"
     :state-path="$getStatePath()"
 >
